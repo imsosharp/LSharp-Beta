39815e077ac2e023b01a5a4adb09c529fa21d482
imsosharp <imsosharp@mail.com>
2015-01-10 12:57:42 +0200

    filehandler test

--- AutoSharpporting/Autoplay/FileHandler.cs
+++ AutoSharpporting/Autoplay/FileHandler.cs
@@ -11,26 +11,26 @@ namespace Support
 {
     internal class FileHandler
     {
-        private static string _cBuildsPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\AutoSharpporting\\"; //y u do dis csharp
+        private static string _cBuildsPath = Config.LeagueSharpDirectory + @"\AutoSharpporting\";
         private static string theFile = ""; //better to intialize at ongameload
         private static string[] _itemsStringArray = { };
         public static int[] Items = { };
 
         public static void DoChecks()
         {
-            theFile = _cBuildsPath + Utility.Map.GetMap().Type.ToString() + Autoplay.Bot.BaseSkinName + ".txt";
+            theFile = _cBuildsPath + Utility.Map.GetMap().Type + Autoplay.Bot.BaseSkinName + ".txt";
             if (!Directory.Exists(_cBuildsPath))
             {
                 Directory.CreateDirectory(_cBuildsPath);
-                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.CrystalScar.ToString());
-                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.HowlingAbyss.ToString());
-                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.SummonersRift.ToString());
-                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.TwistedTreeline.ToString());
-                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.Unknown.ToString());
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.CrystalScar);
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.HowlingAbyss);
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.SummonersRift);
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.TwistedTreeline);
+                Directory.CreateDirectory(_cBuildsPath + Utility.Map.MapType.Unknown);
             }
             if (ExistsCustomBuild())
             {
-                Console.WriteLine("Found custom build");
+                Game.PrintChat("Found custom build");
                 var contents = File.ReadAllText(theFile);
                 string[] separator = { "," };
                 _itemsStringArray = contents.Split(separator, StringSplitOptions.RemoveEmptyEntries);
@@ -43,7 +43,7 @@ namespace Support
 
         public static bool ExistsCustomBuild()
         {
-            return (File.Exists(theFile));
+            return File.Exists(theFile);
         }
 
         public static ItemId[] GetCustomBuild()
